923f843420f1302b95f507cbcd5aa56ff53e230f
Lary-Alecrim <larygamaalecrim2008@gmail.com>
2025-10-04 13:30:38 -0300

    atualizações

--- ALUNOS/sidebar.js
+++ ALUNOS/sidebar.js
@@ -1,8 +1,9 @@
 document
-  .querySelector(".aside-list")
-  .addEventListener("click", function (event) {
+  .querySelectorAll(".aside-list").forEach((list) => {
+  list.addEventListener("click", function (event) {
     const li = event.target.closest("li"); //caça o li
     if (li && li.dataset.link) {
       window.location.href = li.dataset.link; // quando clicar vai mudar para outra página
     }
   });
+});
